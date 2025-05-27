10369d45442d5161e95bee87e5324614e3fe748c
Segn Lee <159688962+XhienLee@users.noreply.github.com>
2025-05-27 10:23:47 +0800

    Delete path.php

--- path.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("ROOT_PATH", __DIR__);
-define("FUNCTION_PATH", ROOT_PATH . "/functions");
-define("DATABASE", ROOT_PATH . "/functions/db_connect.php");
-//define("MODULE", ROOT_PATH . "/module"); // for test
-?>
